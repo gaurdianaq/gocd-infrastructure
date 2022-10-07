6980b1942907fe1f6d79439b57bf312a16415afd
Evan <gaurdianaq@protonmail.com>
2022-10-07 13:55:22 -0400

    terraform and packer scripts

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # gocd-infrastructure
+
+export AWS_ACCESS_KEY_ID=<your_access_key>
+export AWS_SECRET_ACCESS_KEY=<your_secret_access_key>
---
+++ terraform/main.tf
@@ -0,0 +1,87 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "us-east-2"
+}
+
+data "aws_ami" "gocd_ami" {
+  owners           = ["self"]
+  most_recent      = true
+
+  filter {
+    name   = "name"
+    values = ["gaurdianaq-cicd-server-*"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+resource "aws_ebs_volume" "artifact_storage" {
+  availability_zone = "us-east-2a"
+  size              = 40
+
+  tags = {
+    Name = "artifacts"
+  }
+}
+
+resource "aws_security_group" "gocd" {
+  name        = "gocd security group"
+
+  ingress {
+    protocol  = "tcp"
+    from_port = 8153
+    to_port   = 8153
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    protocol = -1
+    from_port = 0
+    to_port = 0
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "expose_8153"
+  }
+}
+
+resource "aws_instance" "cicd_server" {
+  ami               = data.aws_ami.gocd_ami.id
+  instance_type     = "t2.medium"
+  availability_zone = "us-east-2a"
+
+  root_block_device {
+    volume_size = 20
+  }
+
+  security_groups = [ aws_security_group.gocd.name ]
+
+  tags = {
+    Name = "GoCD Server"
+  }
+}
+
+resource "aws_volume_attachment" "ebs_att" {
+  device_name = "/dev/sdh"
+  volume_id   = aws_ebs_volume.artifact_storage.id
+  instance_id = aws_instance.cicd_server.id
+}
